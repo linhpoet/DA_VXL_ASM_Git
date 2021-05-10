d08ce152c0a910da5f528449f5a6456e986b0967
LAPTOP-A76MUA6E\Linh <lequanglinh1412@gmail.com>
2021-05-10 16:54:20 +0700

    no message

--- newAsmTemplate.asm
+++ newAsmTemplate.asm
@@ -65,6 +65,22 @@ muc
 muc_temp
 vong
 vong2
+cntH
+cntL
+hesoH
+hesoL
+rate0
+rate1
+rate2
+rate3
+TOCDO               ;
+TUSO
+NHO1
+NHO2                ;
+TRAM
+CHUC
+DONVI               ;
+THUONGSO
 endc
 #define RS PORTC,5
 #define RW PORTC,6
@@ -97,9 +113,10 @@ RETFIE                      ;The ?return from interrupt? instruction, RETFIE, ex
                             ;if priority levels are used), which re-enables interrupts
 
 
-org 0x50
+org 0x100
 main
 clrf TRISC
+bsf TRISC,0
 clrf TRISD                  ;portD output
 movlw 0xff
 movwf TRISB                 ;portB input
@@ -112,6 +129,13 @@ bsf INTCON2,1               ;RBIP = 1, high priority interrupt PortB
 ;lm298_configuration
 bsf PORTC, 4                ;ENA = ENB = 5v
 bcf PORTC, 3                ;IN2 = 0
+;Timer 1 config-counter doc xung
+movlw 0x03
+movwf T1CON
+movlw 0x00
+movwf TMR1L
+movlw 0x00
+movwf TMR1H
 ;khoi tao lcd
 movlw 0x38                  ;Mode8bit2line
 call ghi_lenh
@@ -122,15 +146,130 @@ call ghi_lenh
 call muc_0
 movff muc_temp, muc
 loop
-movlw 0x00
-movwf T1CON
+;doc xung ve tren encoder trên xung rc0
+clrf TMR1L
+clrf TMR1H
+;delay 1s
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+call delay2
+movff TMR1L, cntL
+movff TMR1H, cntH
+movlw 0xc0              ;xuong hang 2
+call ghi_lenh
+call tinh_toc_do
+MOVF rate1, W
+MOVWF TOCDO
+MOVFF	TOCDO, TUSO	; TUSO = TOCDO
+CLRF		NHO1
+CLRF		NHO2
+CLRF		TRAM
+CLRF		CHUC
+CLRF		DONVI
+CLRF		THUONGSO
+CALL 		CHUYENDOITRAM
+MOVF		THUONGSO, W
+ADDLW	0X30
+MOVWF	TRAM
+movf    TRAM, W
+call ghi_dulieu
+CLRF		THUONGSO
+CALL		CHUYENDOICHUC
+MOVF		THUONGSO, W
+ADDLW	0X30
+MOVWF	CHUC
+movf CHUC, W
+call ghi_dulieu
+MOVF		NHO2, W
+ADDLW	0X30
+MOVWF	DONVI
+movf    DONVI, W
+call ghi_dulieu
+movlw ' '
+call ghi_dulieu
+movlw 'r'
+call ghi_dulieu
+movlw 'p'
+call ghi_dulieu
+movlw 'm'
+call ghi_dulieu
 goto loop
+
+
+tinh_toc_do                 ;(cntH<<8 | cntL)*60/374*2^8 = (cntH<<8 | cntL)*41
+movlw 0x29
+movwf hesoL
+movlw 0x00
+movwf hesoH
+;1L*2L
+movf cntL, W
+mulwf hesoL
+movff PRODH, rate1
+movff PRODL, rate0
+;1H*2L*2^8
+movf cntH, W
+mulwf hesoL
+movf PRODL, W
+addwf rate1, F
+movff PRODH, rate2
+clrf WREG
+addwfc rate2, F               ;W+ rate2 + C_flag = rate2
+;1L*2H*2^8
+movf cntL, W
+mulwf hesoH
+movf PRODL,W
+addwf rate1, F
+movf PRODH, W
+addwf rate2, F
+clrf WREG
+addwfc rate2, F
+;1H*2H*2^16
+movf cntH, W
+mulwf hesoH
+movf PRODL, W
+addwf rate2, F
+movff PRODH, rate3
+clrf WREG
+addwfc rate3, F
+return
+
+CHUYENDOITRAM
+MOVFF	TUSO, NHO1
+MOVLW	B'01100100'		; W = 100
+SUBWF	TUSO,F		; TU SO -W
+BC      		LAP1
+RETURN
+;------------------------------------------------
+CHUYENDOICHUC
+MOVFF	NHO1, NHO2
+MOVLW	B'00001010'		; W=10
+SUBWF	NHO1,F		; nho1 = nho1 - w
+BC      LAP2
+RETURN
+
+
+;------------------------------------------------
+LAP1
+INCF		THUONGSO, F	; TANG THUONG SO CHO MOI LAN THUC HIEN PHEP TRU
+GOTO		CHUYENDOITRAM	; NEU C = 0 QUAY LAI
+;------------------------------------------------
+LAP2
+INCF		THUONGSO, F	     	; TANG THUONG SO CHO MOI LAN THUC HIEN PHEP TRU
+GOTO		CHUYENDOICHUC	; NEU C = 0 QUAY LAI
+
 
 
 start
 movf muc,0                  ;copy muc into W
 xorlw D'0'                  ;xor W with 0; if W=0, Z=1 else Z=0
-btfsc STATUS,Z              ;skip next instruction if Z=0
+btfsc STATUS,Z              ;skip next instruction if Z=0(muc != 0)
 call muc_1                  ;so if muc=0, call muc_1
 ;tuong tu phia tren
 movf muc,0
@@ -181,13 +320,15 @@ movlw 0x00
 movwf muc_temp
 movlw 0x01                  ;clear lcd
 call ghi_lenh
+;bcf T2CON, 2                ;timer 2 off
+;bcf PORTC, 2
 movlw 0x63
 movwf PR2
 movlw 0x0c
 movwf CCP1CON
-movlw 0x21
-movwf CCPR1L
 movlw 0x00
+movwf CCPR1L
+movlw 0x05
 movwf T2CON
 movlw 'C'
 call ghi_dulieu
@@ -302,7 +443,7 @@ movlw 0x63
 movwf PR2
 movlw 0x0c
 movwf CCP1CON
-movlw 0x41
+movlw 0x42
 movwf CCPR1L
 movlw 0x05
 movwf T2CON
@@ -384,10 +525,3 @@ goto lap2
 RETURN
 
 end
-
-
-
-
-
-
-
